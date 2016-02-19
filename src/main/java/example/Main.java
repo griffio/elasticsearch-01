c4f03edcdef93f45d6716f5c8af416684e322a4c
griffio <griffio@users.noreply.github.com>
2016-02-19 10:14:03 +0000

    separate out classes

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -20,25 +20,21 @@ public class Main {
     String httpsURL = "FIXME!";
 
     OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder()
-        .url(httpsURL)
-        .header("Authorization", basicAuth)
-        .build();
-
+    Request.Builder requestBuilder = new Request.Builder();
+    requestBuilder.url(httpsURL).header("Authorization", basicAuth);
+    Request request = requestBuilder.build();
     String statusResponse = client.newCall(request).execute().body().string();
 
     System.out.println("status = " + statusResponse);
 
-    Gson gson;
     GsonBuilder gsonBuilder = new GsonBuilder();
-    gson = gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
+    gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+    Gson gson = gsonBuilder.create();
+    GsonConverterFactory converterFactory = GsonConverterFactory.create(gson);
 
-    Retrofit retrofit = new Retrofit.Builder()
-        .client(client)
-        .baseUrl(httpsURL)
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build();
-
+    Retrofit.Builder retrofitBuilder = new Retrofit.Builder();
+    retrofitBuilder.baseUrl(httpsURL).client(client).addConverterFactory(converterFactory).build();
+    Retrofit retrofit = retrofitBuilder.build();
     ClusterQuery query = retrofit.create(ClusterQuery.class);
 
     Call<ClusterStatus> queryCall = query.getStatus(basicAuth);
